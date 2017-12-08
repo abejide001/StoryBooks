39e705b4b2592d8ca4843e88040735d40ebd94ef
Anthony Quintero-Quiroga <anthony.quiroga@gmail.com>
2017-12-07 17:19:33 -0800

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://amquinte:dev@ds133856.mlab.com:33856/storybooks-dev',
+    googleClientID: '370790878312-g42abv2qvic7quueaencj6p61q769hc5.apps.googleusercontent.com',
+    googleClientSecret: 'LG8l7s8rn-HUfPb4DdC4VRxH'
+}
